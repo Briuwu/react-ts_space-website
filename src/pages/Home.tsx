b58739666b441fabd593ab6fe71b0aed12698ed7
Briuwu <millontebrian@gmail.com>
2023-02-16 17:20:31 +0800

    added a home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Section from "../components/Section";
 
 const Home = () => {
-  return <Section classProps="home">Home</Section>;
+  return (
+    <Section classProps="home">
+      <div className="home__desc">
+        <p className="home__desc-subhead">So, you want to travel to</p>
+        <h1 className="home__desc-heading">space</h1>
+        <p className="home__desc-info">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <Link to="/destination" className="home__explore">
+        <span>explore</span>
+      </Link>
+    </Section>
+  );
 };
 
 export default Home;
