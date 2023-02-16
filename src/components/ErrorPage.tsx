bb56ef71713e1a59b29a16311dcf2f73c1affbcc
Briuwu <millontebrian@gmail.com>
2023-02-16 19:31:41 +0800

    added the error page.

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="error">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occured.</p>
+      <Link to="/home">Go back to home page.</Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
