7b454939de92492c125454f0b694470aaac362c3
Briuwu <millontebrian@gmail.com>
2023-02-19 19:10:07 +0800

    added the Crew

--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import Section from "../components/Section";
 
 const Crew = () => {
-  return <Section classProps="crew">Crew</Section>;
+  return (
+    <Section classProps="crew">
+      <h1 className="section__title">
+        <span aria-hidden="true">02</span>meet your crew
+      </h1>
+    </Section>
+  );
 };
 
 export default Crew;
